cd4ac47bc0495985a8b617dbae67b499599c5fce
vyshu <vyshu.tadipaneni14@gmail.com>
2022-02-04 13:57:58 -0600

    added files

--- mapper.py
+++ mapper.py
@@ -4,7 +4,7 @@ import sys
 for line in sys.stdin:
   datalist = line.strip().split(",")
   if (len(datalist) == 9) : 
-    Country_Name,Covid_19Deaths,Cardiovascular_diseases,Respiratory_diseases ,Kidney_diseases,Neonatal_disorders ,Meningitis ,Malaria ,Interpersonal_violence = datalist = datalist
+    Country_Name,Covid_19Deaths,Cardiovascular_diseases,Respiratory_diseases ,Kidney_diseases,Neonatal_disorders ,Meningitis ,Malaria ,Interpersonal_violence , = datalist
 
     # print intermediate key-value pairs to standard output
     print(Country_Name,"\t",Covid_19Deaths)
